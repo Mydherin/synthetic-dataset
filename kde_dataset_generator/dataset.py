30aed72e82d407120eec9810b7b950b5f1dfff82
Mydherin <mydherin@gmail.com>
2022-06-16 18:26:25 +0200

    Plot univariate dataset distribution by category using KDE

--- kde_dataset_generator/dataset.py
+++ kde_dataset_generator/dataset.py
@@ -1,12 +1,26 @@
 from pandas import DataFrame
 from sklearn.datasets import make_blobs
+import matplotlib.pyplot as plt
 
 # Generate a random dataset with 1 feature
-def generate_test_dataset(n_samples, n_categories, seed):
+def generate_univariate_dataset(n_samples, n_categories, seed):
     # Define feature number
     n_features = 1
     # Generate data with X,y format
-    X, y = make_blobs(n_samples=300, centers=2, n_features=n_features, random_state=1)
+    X, y = make_blobs(n_samples=n_samples, centers=n_categories, n_features=n_features, random_state=seed)
     # Create dataframe
     df = DataFrame(dict(x=X[:,0], category=y))
     return df
+
+# Plot an histogram showing univariate dataset distribution
+def plot_univariate(df):
+    # Define figure
+    fig, ax = plt.subplots()
+    # Get categories values
+    categories = df.iloc[:,-1].unique()
+    # Plot hist by category
+    for category in categories:
+        # Get distribution values
+        values = df[df.iloc[:,1] == category].iloc[:,0]
+        # Plot distribution
+        ax.hist(values)
--- kde_dataset_generator/kde.py
+++ kde_dataset_generator/kde.py
@@ -1,4 +1,6 @@
+import numpy as np
 from scipy import stats
+import matplotlib.pyplot as plt
 
 # Calculate KDE by category
 def calculate_kdes(df):
@@ -13,6 +15,32 @@ def calculate_kdes(df):
         values = filtered_df.iloc[:,:-1].to_numpy()
         # Calculate KDE
         kde = stats.gaussian_kde(values.T)
+        # TMP
+        # min = np.min(values)
+        # max = np.max(values)
+        # support = np.linspace(min, max, 1000)
+        # density = kde(support)
+        # plt.fill_between(support, density)
         # Add KDE to KDEs dictionary
         kdes[category] = kde
     return kdes
+
+# Plot univariate dataset using KDE
+def plot_univariate(df, kdes):
+    # Define figure
+    fig, ax = plt.subplots()
+    # Plot by category
+    for category in kdes:
+        # Get distribution values
+        values = df[df.iloc[:,1] == category].iloc[:,0]
+        # Get min and max values
+        min = values.min()
+        max = values.max()
+        # Generate support
+        support = np.linspace(min, max, 1000)
+        # Get category KDE
+        kde = kdes[category]
+        # Get KDE density 
+        density = kde(support)
+        # Plot distribution
+        ax.fill_between(support, density)
