66123f6e30c5788990d3cdb5c3551f0f6d5c8a0e
Mydherin <mydherin@gmail.com>
2022-06-18 13:50:49 +0200

    Calculate densities for each dataset

--- kde_dataset_generator/kde.py
+++ kde_dataset_generator/kde.py
@@ -1,3 +1,4 @@
+import pandas as pd
 import numpy as np
 from scipy import stats
 import matplotlib.pyplot as plt
@@ -25,6 +26,37 @@ def calculate_kdes(df):
         kdes[category] = kde
     return kdes
 
+# Estimate instance densities using KDEs
+def estimate_density(df, kdes):
+    # Define new df
+    new_df = []
+    # Get categories
+    categories = df.iloc[:,-1].unique()
+    # Estimate instance density for each category instances
+    for category in categories:
+        # Filter dataset by category
+        filtered_df = df[df.iloc[:,-1] == category]
+        # Get category kde
+        kde = kdes[category]
+        # Estimate density for each instance in filtered dataset
+        for index, row in df.iterrows():
+            # Instance without category to numpy
+            instance = row.iloc[:-1].to_numpy()
+            # Estimate density
+            density = kde(instance)[0]
+            # Instance with category to list
+            instance = row.to_list()
+            # Add density to instance
+            instance.append(density) 
+            # Add instance to new df
+            new_df.append(instance)
+    # Define new dataset columns
+    columns = df.columns.to_list()
+    columns.append("density")
+    # Create new df from new df instances
+    new_df = pd.DataFrame(data=new_df, columns=columns)
+    return new_df
+
 # Plot univariate dataset with multiplet categories using KDE
 def plot_univariate(df, kdes):
     # Define figure
@@ -44,19 +76,3 @@ def plot_univariate(df, kdes):
         density = kde(support)
         # Plot distribution
         ax.fill_between(support, density)
-
-# Plot univariate dataset with single categories using KDE
-def plot_univariate_single(df, kde):
-    # Define figure
-    fig, ax = plt.subplots()
-    # Get distribution values
-    values = df.iloc[:,0]
-    # Get min and max values
-    min = values.min()
-    max = values.max()
-    # Generate support
-    support = np.linspace(min, max, 1000)
-    # Get KDE density 
-    density = kde(support)
-    # Plot distribution
-    ax.fill_between(support, density, color="k")
